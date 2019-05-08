6bdfcc6d35de41b6bef7fffa03bac3cdd1d2e9e9
chesterone <373934650@qq.com>
2019-05-08 20:49:52 +0800

    sample_delegate

---
+++ Samples4CSharp/Samples4CSharp/Sample_delegate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample_delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
