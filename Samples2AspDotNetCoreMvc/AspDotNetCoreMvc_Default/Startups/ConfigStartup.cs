ec95e706b71ebde5845316dac9c706a6a2d4bd80
chester <373934650@qq.com>
2019-05-17 17:46:11 +0800

    Add files via upload

---
+++ Samples2AspDotNetCoreMvc/AspDotNetCoreMvc_Default/Startups/ConfigStartup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspDotNetCoreMvc_Default.Startups
+{
+    public class ConfigStartup : IStartup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
